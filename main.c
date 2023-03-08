ef4d80bfba84c491eb348334dfb9c343f3b12d40
Jakub Iwaszkiewicz <102925580+jakubiwaszkiewicz@users.noreply.github.com>
2023-03-08 16:00:26 +0100

    add/initialize the repo

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    char characterName[] = "John";
+    int characterAge = 35;
+
+    printf("There once was a man named %s\n", characterName);
+    printf("he was %d years old.\n", characterAge);
+    printf("He really like the name %s\n", characterName);
+    printf("but did not like being %d.\n", characterAge);
+    return 0;
+}
